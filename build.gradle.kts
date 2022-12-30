067c21cfd76cf247fbb2b4eff46dd37e9cb1d134
iseki <admin@iseki.space>
2022-12-30 17:08:14 +0800

    chore: license URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ publishing {
                 licenses {
                     license {
                         name.set("MIT")
+                        url.set("https://github.com/iseki0/envproxyselector/raw/master/LICENSE")
                     }
                 }
                 developers {
